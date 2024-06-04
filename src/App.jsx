494375f3ddf2a65560cef8ad192f6588ffcff8d6
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-06-04 14:03:38 +0700

    style: added styles to buttons and alert boxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
+import Buttons from './components/Button.jsx';
+import Alert from './components/Alert.jsx';
 
 function App() {
+
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+        <Buttons priority="primary"/>
+        <br />
+        <Buttons priority="secondary"/>
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+      <Alert status="error"/>
+      <Alert status="warning"/>
+      <Alert status="info"/>
+      <Alert status="success"/>
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,20 @@
 // Start coding here
 
-function Button() {}
+function Buttons(props){
+    const priority = props.priority;
+    if ( priority === "primary"){
+      return (
+        <button className="primary-button">
+          Buttons
+        </button>
+      )
+    } else if (priority === "secondary"){
+      return (
+        <button className="secondary-button">
+          Buttons
+        </button>
+      )
+    }
+  }
+
+export default Buttons;
