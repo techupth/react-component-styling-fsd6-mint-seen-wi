61837728b167608dc6f584c1678e15cfaeca0812
wiKanok <kanokwan1401.ks@gmail.com>
2024-06-04 14:36:19 +0700

    feat: wi-react-component-styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,45 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <>
+        <button
+          css={css`
+            width: 170px;
+            height: 50px;
+            color: white;
+            background-color: #074ee8;
+            outline: none;
+            border: none;
+            padding: 0px, 16px, 0px, 16px;
+            border-radius: 4px;
+          `}
+        >
+          Large
+        </button>
+      </>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <button
+        css={css`
+          width: 170px;
+          height: 50px;
+          color: white;
+          background-color: #07a4e8;
+          outline: none;
+          border: none;
+          padding: 0px, 16px, 0px, 16px;
+          border-radius: 4px;
+        `}
+      >
+        Large
+      </button>
+    );
+  }
+}
+
+export default Button;
